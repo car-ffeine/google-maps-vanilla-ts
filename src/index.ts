9774609d979692b47130ee498d665cc68496f55b
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-29 00:09:47 +0900

    refactor: 키에 .env 적용

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import './index.css'
+import './index.css';
 
-let map: google.maps.Map;
+declare global {
+  interface Window {
+    google: any;
+  }
+}
+
 async function initMap(): Promise<void> {
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { Map } = window.google.maps;
+  let map: google.maps.Map;
+
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
 
-initMap();
+function loadGoogleMapsAPI(): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&callback=initMap`;
+    script.onerror = () => reject(new Error("Failed to load Google Maps API"));
+    script.onload = () => resolve();
+    document.head.appendChild(script);
+  });
+}
+
+
+loadGoogleMapsAPI()
+  .then(() => initMap())
+  .catch((error) => console.error(error));
