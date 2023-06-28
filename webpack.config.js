d7e54265544ac2cdfcc3638739d2206dffe19df1
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-28 19:24:09 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotEnv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    historyApiFallback: true,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new DotEnv(),
+    // new CopyPlugin({
+    //   patterns: [{ from: "src/assets", to: "assets" }],
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|ts)$/i,
+        exclude: /node_modules/,
+        use: { loader: "ts-loader" },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
